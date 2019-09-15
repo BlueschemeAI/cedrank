0bb7159a823205a7832e1971daab4e6ed60f6c6c
sujon-kutumbita <sujon@kutumbita.com>
2019-09-15 15:00:00 +0600

    Exeption handleing

--- cedrank/agency/internal_requests.py
+++ cedrank/agency/internal_requests.py
@@ -2,8 +2,11 @@ from cedrank.cedrank.settings import client, from_number
 
 
 def send_sms_to(worker_phone, message_body):
-    client.messages.create(
-        body=message_body,
-        from_=from_number,
-        to=worker_phone
-    )
+    try:
+        client.messages.create(
+            body=message_body,
+            from_=from_number,
+            to=worker_phone
+        )
+    except:
+        print('Message not send.')
